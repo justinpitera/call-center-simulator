8bf7b1b8c5d6e6968ff679ea3a43905af8f56cdd
justinpitera@gmail.com <justinpitera@gmail.com>
2023-03-08 16:35:40 -0500

    Messing around with averages, wanted to pull it on my desktop

--- Driver.py
+++ Driver.py
@@ -1,12 +1,16 @@
 import csv
+from io import StringIO
 from multiprocessing.connection import answer_challenge
-
+from os import system
+from sre_constants import IN
 
 incoming_calls = []
-answer_calls = []
+answered_calls = []
 abandonded_calls = []
+answer_speed = []
 
 #  CSV FORMAT
+#    0          1             2             3            4               5                    6                   7                    8
 #  Index,Incoming Calls,Answered Calls,Answer Rate,Abandoned Calls,Answer Speed (AVG),Talk Duration (AVG),Waiting Time (AVG),Service Level (20 Seconds)
 #  Open sample data file
 with open('data.csv', 'r') as csvfile:
@@ -15,11 +19,12 @@ with open('data.csv', 'r') as csvfile:
 
     for row in reader:
         incoming_calls.append(int(row[1]))
-        answer_calls.append(int(row[2]))
+        answered_calls.append(int(row[2]))
         abandonded_calls.append(int(row[4]))
+        answer_speed.append((row[5]))
 
 
-
-print(incoming_calls)
-print(answer_calls)
-print(abandonded_calls)
+selection = int(input())
+print(answered_calls[selection]/incoming_calls[selection])
+print(abandonded_calls[selection]/incoming_calls[selection])
+print("Average answer speed: " + answer_speed[selection])

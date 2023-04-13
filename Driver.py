1e108b108fd0c4a8a46edba0631d4171bb6b5980
mtobino <tobino68@students.rowan.edu>
2023-04-13 15:08:13 -0400

    trying to get the customer class field to be based off data from the csv file

--- Driver.py
+++ Driver.py
@@ -15,10 +15,17 @@ def toSeconds(time):
 
 def randomD(min, max):
     return random.uniform(min, max)
+
+index = []
 incoming_calls = []
 answer_calls = []
+answer_rate = []
 abandonded_calls = []
 answer_speed = []
+talk_duration = []
+waiting_time =[]
+service_level = []
+
 
 #  CSV FORMAT
 #  Index,Incoming Calls,Answered Calls,Answer Rate,Abandoned Calls,Answer Speed (AVG),Talk Duration (AVG),Waiting Time (AVG),Service Level (20 Seconds)
@@ -28,29 +35,37 @@ with open('data.csv', 'r') as csvfile:
     #next(reader)
 
     for row in reader:
+        index.append(int(row[0]))
         incoming_calls.append(int(row[1]))
         answer_calls.append(int(row[2]))
+        answer_rate.append(str(row[3]))
         abandonded_calls.append(int(row[4]))
         answer_speed.append(toSeconds(row[5]))
+        talk_duration.append(toSeconds(row[6]))
+        waiting_time.append(toSeconds(row[7]))
+        service_level.append(str(row[8]))
 
 avg = sum(abandonded_calls) / len(abandonded_calls)
 numofA = len(abandonded_calls)
 answerSpeedSecondList = list()
 rand = random.randint(0, max(answer_speed))
-print(rand)
+#print(rand)
+
+randomDay = random.randint(min(index), max(index))
+randomServiceTime = random.randint(min(talk_duration), max(talk_duration))
+customerCount = int(input("how many customers for this simulation: "))
 
 customerEntered = 0.0
 customerList = []
-for i in range(20):
+for i in range(customerCount):
     num = random.randint(0, 4)
     if num == 4:
-        cus = CustomerClass.Customer(i, randomD(0.2, 1.3) + customerEntered, randomD(2.7,3.1))
+        cus = CustomerClass.Customer(i, randomD(0.2, 1.3) + customerEntered, randomD(0, randomServiceTime + 2))
     else:
-        cus = CustomerClass.Customer(i, randomD(0.2, 1.3) + customerEntered, randomD(1.0, 2.6))
+        cus = CustomerClass.Customer(i, randomD(0.2, 1.3) + customerEntered, randomD(0, randomServiceTime))
     customerEntered = cus.getEntryTime()
     customerList.append(cus)
 
-print(customerList)
 customerList[0].setServiceStartTime(customerList[0].getEntryTime())
 customerList[0].setServiceEndTime(customerList[0].getServiceStartTime() + customerList[0].getServiceTime())
 print(customerList[0].toString())
@@ -58,9 +73,11 @@ print(customerList[0].toString())
 for i in range(1, len(customerList)):
     cus = customerList[i]
     prevCus = customerList[i-1]
-    
+
     cus.setServiceStartTime(prevCus.getServiceEndTime())
     cus.setServiceEndTime(cus.getServiceStartTime() + cus.getServiceTime())
     print(cus.toString())
+
+
 
 

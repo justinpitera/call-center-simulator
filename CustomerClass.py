8c09d75395c68aea15fde6c3ff1eddaf77e6268d
mtobino <tobino68@students.rowan.edu>
2023-04-13 14:15:28 -0400

    Recreated Assignment 4 in python

--- CustomerClass.py
+++ CustomerClass.py
@@ -12,9 +12,9 @@ class Customer:
     serviceEndTime = None
 
     def __init__(self, id, entryTime, serviceTime) -> None:
-        self.serviceTime = serviceTime
+        self.serviceTime = round(serviceTime,2)
         self.id = id
-        self.entryTime = entryTime
+        self.entryTime = round(entryTime,2)
         pass
     
     def getId(self):
@@ -32,10 +32,11 @@ class Customer:
     def getServiceEndTime(self):
         return self.serviceEndTime
 
-    def setStartTime(self, startTime):
-        self.serviceStartTime = startTime
+    def setServiceStartTime(self, startTime):
+        self.serviceStartTime = round(startTime,2)
     
     def setServiceEndTime(self, endTime):
-        self.serviceEndTime = endTime
+        self.serviceEndTime = round(endTime,2)
 
-    
+    def toString(self):
+        return "Customer [id=" + str(self.id) + ", entryTime=" + str(self.entryTime) + ", startTime= " + str(self.serviceStartTime) + ", serviceTime=" + str(self.serviceTime) + ", DepartureTime=" + str(self.serviceEndTime) + "]"
--- Driver.py
+++ Driver.py
@@ -1,6 +1,7 @@
 import csv
 from multiprocessing.connection import answer_challenge
 import CustomerClass
+import random
 
 
 ## take in time string formatted as 00:00:00 and convert it to total seconds
@@ -12,6 +13,8 @@ def toSeconds(time):
     seconds = int(numbers[2])
     return hours*3600 + minutes*60 + seconds
 
+def randomD(min, max):
+    return random.uniform(min, max)
 incoming_calls = []
 answer_calls = []
 abandonded_calls = []
@@ -29,9 +32,35 @@ with open('data.csv', 'r') as csvfile:
         answer_calls.append(int(row[2]))
         abandonded_calls.append(int(row[4]))
         answer_speed.append(toSeconds(row[5]))
-avg = sum(incoming_calls) / len(incoming_calls)
-#print(incoming_calls)
-print(avg)
-#print(answer_calls)
-#print(abandonded_calls)
-#print(answer_speed)
+
+avg = sum(abandonded_calls) / len(abandonded_calls)
+numofA = len(abandonded_calls)
+answerSpeedSecondList = list()
+rand = random.randint(0, max(answer_speed))
+print(rand)
+
+customerEntered = 0.0
+customerList = []
+for i in range(20):
+    num = random.randint(0, 4)
+    if num == 4:
+        cus = CustomerClass.Customer(i, randomD(0.2, 1.3) + customerEntered, randomD(2.7,3.1))
+    else:
+        cus = CustomerClass.Customer(i, randomD(0.2, 1.3) + customerEntered, randomD(1.0, 2.6))
+    customerEntered = cus.getEntryTime()
+    customerList.append(cus)
+
+print(customerList)
+customerList[0].setServiceStartTime(customerList[0].getEntryTime())
+customerList[0].setServiceEndTime(customerList[0].getServiceStartTime() + customerList[0].getServiceTime())
+print(customerList[0].toString())
+
+for i in range(1, len(customerList)):
+    cus = customerList[i]
+    prevCus = customerList[i-1]
+    
+    cus.setServiceStartTime(prevCus.getServiceEndTime())
+    cus.setServiceEndTime(cus.getServiceStartTime() + cus.getServiceTime())
+    print(cus.toString())
+
+
